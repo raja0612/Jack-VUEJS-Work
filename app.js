8ab4300d14a7b1d8a42838445dc4018be103f40b
raja0612 <rajashekhar.ramayampeta@gmail.com>
2018-12-07 18:03:27 -0500

    added quiz json file

--- app.js
+++ app.js
@@ -7,5 +7,17 @@ new Vue({
     },
     created () {
       console.log('created')
+      this.loadQuiz();
+    },
+    methods: {
+      loadQuiz () {
+        fetch('https://github.com/raja0612/Jack-VUEJS-Work/blob/master/app.json',{mode: 'no-cors'})
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(myJson) {
+          console.log(JSON.stringify(myJson));
+        });
+      }
     }
 })

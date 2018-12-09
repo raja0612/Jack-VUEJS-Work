204c2eb14b77411e2b62136e540046544fe140d8
raja0612 <rajashekhar.ramayampeta@gmail.com>
2018-12-09 12:20:26 -0500

    added code to shuffle quiz questions and options

--- app.js
+++ app.js
@@ -1,68 +1,94 @@
 new Vue({
   el: '#app',
   data: {
-    name:'QUIZ',
+    name: 'QUIZ',
     quiz: [],
     questionNumber: 0,
     showResults: false,
     results: [],
     quizCompleted: '',
-    herokuURL:'https://fierce-everglades-50669.herokuapp.com/'
+    quizKeyUrl: 'https://fierce-everglades-50669.herokuapp.com/',
+    //quizKeyUrl: 'http://localhost:5000/',
+    retry: false
   },
-  mounted () {
+  mounted() {
     console.log('mounted')
-    axios.get('https://raw.githubusercontent.com/raja0612/Jack-VUEJS-Work/master/app.json',
-    {crossdomain: true})
-    .then(response => {
-      console.log('response', response.data)
-      this.quiz = response.data.data
-      console.log('this.quiz ', this.quiz)
-      console.log('this.quiz.length ', this.quiz.length)
-    })
-    .catch(error => {
-      console.log(error);
-    });
+    axios.get('https://raw.githubusercontent.com/raja0612/Jack-VUEJS-Work/master/quiz.json', {
+        crossdomain: true
+      })
+      .then(response => {
+        console.log('response', response.data)
+        this.quiz = response.data.data
+        console.log('this.quiz ', this.quiz)
+        console.log('this.quiz.length ', this.quiz.length)
+        this.shuffleQuiz();
+      })
+      .catch(error => {
+        console.log(error);
+      });
   },
-  methods : {
-    userAnswer (answer, questionNumber, question){
-      console.log('userAnswer....', answer, questionNumber,question)
+  methods: {
+    shuffleQuiz() {
+      // First shuffle quiz questions
+      this.quiz.sort(function () {
+        return .5 - Math.random();
+      });
+
+      // Then Shuffle Quiz options 
+      this.quiz.forEach(quiz => {
+        quiz.options.sort(function () {
+          return .5 - Math.random();
+        });
+      });
+    },
+    userAnswer(answer, questionNumber, id, question) {
+      console.log('userAnswer....', answer, questionNumber, id, question)
       console.log('userAnswers....', this.userAnswers)
-      if (questionNumber === this.quiz.length-1){
-         console.log('Need to show Results page')
-         this.showResults = true
-         this.quizCompleted = 'Congratulations...! You have Sucessfully completed the QUIZ.' 
-                               +'Here is your Results'
+      if (questionNumber === this.quiz.length - 1) {
+        console.log('Need to show Results page')
+        this.showResults = true
+        this.quizCompleted = 'Congratulations...! You have Sucessfully completed the QUIZ.' +
+          'Here is your Results'
       } else {
         console.log('still questions are left in quiz')
         this.questionNumber++
       }
 
-      if (answer === this.quiz[questionNumber].answer) {
-         console.log('user answer is correct')
-         let result = {
-           'questionNumber': questionNumber,
-           'question': question,
-           'userAnswer': answer,
-           'result': true
-         }
-         this.results.push(result)
-      } else {
-        console.log('user answer is not correct')
-        let result = {
-          'questionNumber': questionNumber,
-          'question': question,
-          'userAnswer': answer,
-          'result': false
-        }
-        this.results.push(result)
-      }
+      axios.get(this.quizKeyUrl + id + '/' + answer, {
+          crossdomain: true,
+        })
+        .then(response => {
+          console.log('response', response.data)
+          if (response.data) {
+            let result = {
+              'questionNumber': questionNumber,
+              'question': question,
+              'userAnswer': answer,
+              'result': response.data
+            }
+            this.results.push(result)
+          } else {
+            // show retry button if user gets wrong answer
+            this.retry = true;
+            let result = {
+              'questionNumber': questionNumber,
+              'question': question,
+              'userAnswer': answer,
+              'result': false
+            }
+            this.results.push(result)
 
+          }
+        })
+        .catch(error => {
+          console.log(error);
+        });
       console.log('results...', this.results)
 
     },
-    reTake () {
+    reTake() {
       location.reload();
     }
   }
-  
+
 });
